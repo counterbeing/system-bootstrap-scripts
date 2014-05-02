2b10c38b6b777787d781f3346b67fb5369a71f57
Cory Logan <cory@counterbeing.com>
2014-05-02 17:01:25 -0400

    rbenv

---
+++ rbenv_install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+exec $SHELL
+
+rbenv install 2.1.1
